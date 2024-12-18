e370f5027526c172852fd3f92250429f951e60f9
HoangVu2216 <vutrananhhoang2216@gmail.com>
2024-12-18 07:32:55 +0700

    Add files via upload
    bài tập 10

---
+++ BT10.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int number;
+    int check = 1;
+    printf("Nhap mot so nguyen duong: ");
+    scanf("%d", &number);
+    for (int i = 2; i <= sqrt(number); i++) {
+        if (number % i == 0) {
+            check = 0;
+            break;
+        }
+    }
+    if (check == 1) {
+        printf("%d la so nguyen to.\n", number);
+    } else {
+        printf("%d khong phai la so nguyen to.\n", number);
+    }
+    return 0;
+}
+
